029cc10a104c91e55a7182cc32673a3a60c438c6
Andrey Kuzmin <unsoundscapes@gmail.com>
2013-10-26 17:48:58 +0100

    Updated map styles

---
+++ public/js/config.js
@@ -0,0 +1,81 @@
+define([ 'async!http://maps.google.com/maps/api/js?v=3.exp&sensor=false&libraries=visualization' ]
+       , function () {
+
+  return {
+    mapOptions: {
+      zoom: 13
+    , center: new google.maps.LatLng(52.51831, 13.404985)
+    //, mapTypeId: google.maps.MapTypeId.SATELLITE
+    , panControl: false
+    , zoomControl: true
+    , zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE
+      , position: google.maps.ControlPosition.MIDDLE_LEFT
+      }
+    , mapTypeControl: false
+    , scaleControl: false
+    , streetViewControl: false
+    , overviewMapControl: false
+    , styles: [
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },{
+        "featureType": "road",
+        "stylers": [
+          { "saturation": 1 },
+          { "hue": "#0077ff" },
+          { "lightness": 100 }
+        ]
+      },{
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "visibility": "on" },
+          { "color": "#c8c8c8" }
+        ]
+      },{
+        "featureType": "landscape.natural",
+        "stylers": [
+          { "color": "#c8c8c8" }
+        ]
+      },{
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "color": "#c8c8c8" }
+        ]
+      },{
+        "elementType": "labels",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      },{
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "color": "#8a8abe" }
+        ]
+      },{
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          { "color": "#000000" },
+          { "weight": 0.1 }
+        ]
+      },{
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          { "weight": 2 },
+          { "color": "#ffff5f" }
+        ]
+      },{
+      }
+    ]
+    }
+  }
+})
--- public/js/views/map.js
+++ public/js/views/map.js
@@ -1,14 +1,8 @@
 define([ 'backbone'
        , 'underscore'
+       , 'config'
        , 'async!http://maps.google.com/maps/api/js?v=3.exp&sensor=false&libraries=visualization'
-       ], function (Backbone, _) {
-
-  var mapOptions = {
-        zoom: 13
-      , center: new google.maps.LatLng(52.51831, 13.404985)
-      , mapTypeId: google.maps.MapTypeId.SATELLITE
-      }
-
+       ], function (Backbone, _, config) {
 
   return Backbone.View.extend({
 
@@ -18,15 +12,16 @@ define([ 'backbone'
     }
 
   , render: function () {
-      this.map = new google.maps.Map(this.el, mapOptions)
+      this.map = new google.maps.Map(this.el, config.mapOptions)
       return this
     }
 
   , addMap: function () {
       this.heatPointArray = new google.maps.MVCArray(this.collection.getPoints())
       this.heatmap = new google.maps.visualization.HeatmapLayer({data: this.heatPointArray})
-      this.heatmap.setOptions({radius: 100});
-      this.heatmap.setOptions({opacity: 0.6});
+      this.heatmap.setOptions({ radius: 100
+                              , opacity: 0.6
+                              })
       this.heatmap.setMap(this.map)
     }
 
